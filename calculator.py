80a9075af98626e9fce895dc8738a787c17573af
Harjeet J L <75530002+Harjeet2411@users.noreply.github.com>
2025-02-24 00:15:21 +0530

    Implemented square root

--- calculator.py
+++ calculator.py
@@ -14,6 +14,10 @@ class Calculator:
     def divide(self, a, b):
         return a / b
 
+#Implement square root function.
+    def square_root(self, x):
+        return math.sqrt(x)
+
 
 if __name__ == "__main__":
 
@@ -24,3 +28,7 @@ if __name__ == "__main__":
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+    
+    #Testing square root feature.
+    num3 = 25
+    print(f"The square root of {num3} = {calculator.square_root(num3)}")
